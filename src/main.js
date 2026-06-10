c2ac6b04d1beea59bec98b099df4e9f52c911614
sodaakim <sohuikim2020@naver.com>
2026-06-10 15:26:45 +0900

    fix: remove hardcoded localhost:8080 baseURL

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "./assets/main.css";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.withCredentials = false;
 
 import { createApp } from "vue";
