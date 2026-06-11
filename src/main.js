6d958973ca76f7dbfcd7d23e4e76233f30169f88
jgs7784 <jgs7784@gmail.com>
2026-06-11 15:15:36 +0900

    fix : team add

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import "./assets/main.css";
 import axios from "axios";
 
 axios.defaults.withCredentials = false;
-
+axios.defaults.baseURL = "http://13.124.245.69:8080";
 import { createApp } from "vue";
 import App from "./App.vue";
 
